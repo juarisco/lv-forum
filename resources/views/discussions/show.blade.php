790c674e721f4c5247c0f3c7ef9b376fdd30e9e0
juarisco <juarisco@outlook.com>
2019-05-08 11:22:18 -0500

    5. Save replies to database

--- resources/views/discussions/show.blade.php
+++ resources/views/discussions/show.blade.php
@@ -15,4 +15,30 @@
         </div>
     </div>
 
+    <div class="card my-5">
+        <div class="card-header">Add a reply</div>
+        <div class="card-body">
+                @auth
+                    <form action="{{ route('replies.store',$discussion->slug) }}" method="post">
+                        @csrf
+
+                        <input type="hidden" name="reply" id="reply">
+                        <trix-editor input="reply"></trix-editor>
+
+                        <button type="submit" class="btn btn-sm btn-success my-2">Add Reply</button>
+                    </form>
+                @else
+                    <a href="{{ route('login') }}" class="btn btn-info">Sign in to add a reply</a>
+                @endauth
+            </div>
+    </div>
+
+@endsection
+
+@section('css')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/trix/1.1.1/trix.css">
+@endsection
+
+@section('js')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/trix/1.1.1/trix.js"></script>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,6 +16,12 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
+    <style>
+        .btn-info{
+            color: white
+        }
+    </style>
+
     @yield('css')
 </head>
 <body>
@@ -71,11 +77,20 @@
             </div>
         </nav>
 
-        @auth
+        {{ request()->path() }}
+        @if (!in_array(request()->path(),['login','register','password/email','password/reset']))
             <main class="container py-4">
                 <div class="row">
                     <div class="col-md-4">
-                        <a href="{{ route('discussions.create') }}" style="width:100%; color:white" class="btn btn-info my-2">Add Discussion</a>
+                        @auth
+                            <a href="{{ route('discussions.create') }}" style="width:100%" class="btn btn-info my-2">
+                                Add Discussion
+                            </a>
+                        @else
+                            <a href="{{ route('login') }}" style="width:100%" class="btn btn-info my-2">
+                                Sign in to add discussion
+                            </a>
+                        @endauth
                         <div class="card">
                             <div class="card-header">Channels</div>
                             <div class="card-body">
@@ -91,12 +106,12 @@
                         @yield('content')
                     </div>                
                 </div>
-            </main>        
+            </main>             
         @else
             <main class="py-4">
                 @yield('content')
-            </main>        
-        @endauth
+            </main>
+        @endif       
     </div>
 
     <!-- Scripts -->
