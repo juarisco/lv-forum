4d14068a683e0fee3e029dc3024f327123fa66a5
juarisco <juarisco@outlook.com>
2019-05-10 15:48:24 -0500

    14.-Session-messages

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -121,6 +121,16 @@
                         </div>
                     </div>
                     <div class="col-md-8">
+                        @if (session()->has('success'))
+                            <div class="alert alert-success">
+                                {{ session()->get('success') }}
+                            </div>
+                        @endif
+                        @if (session()->has('error'))
+                            <div class="alert alert-danger">
+                                {{ session()->get('error') }}
+                            </div>
+                        @endif                        
                         @yield('content')
                     </div>                
                 </div>
