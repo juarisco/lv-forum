636747b2c4f86bf5b125901d5cb6966857f23306
juarisco <juarisco@outlook.com>
2019-05-10 14:35:35 -0500

    12. Filter discussions by channel

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -110,7 +110,11 @@
                             <div class="card-body">
                                 <ul class="list-group">
                                     @foreach ($channels as $channel)
-                                        <li class="list-group-item">{{ $channel->name }}</li>
+                                        <li class="list-group-item">
+                                            <a href="{{ route('discussions.index') }}?channel={{ $channel->slug }}">
+                                                {{ $channel->name }}
+                                            </a>
+                                        </li>
                                     @endforeach
                                 </ul>  
                             </div>
